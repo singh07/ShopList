531eaa57962c7e16c49003bddefffca5a05603a3
akash singh <ak434640@gmail.com>
2014-04-03 21:54:41 +0530

    init

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,25 +1,22 @@
-# --- Created by Ebean DDL
-# To stop Ebean DDL generation, remove this comment and start using Evolutions
-
 # --- !Ups
 
 create table member (
-  email                     varchar(255) not null,
+  email                     varchar(255) PRIMARY KEY,
   password                  varchar(255),
   first_name                varchar(255),
   last_name                 varchar(255),
-  constraint pk_member primary key (email))
+  )
 ;
 
 create table product (
-  id                        bigint not null,
+  id                        bigserial PRIMARY KEY,
   name                      varchar(255),
-  price                     float,
-  constraint pk_product primary key (id))
+  price                     real,
+  )
 ;
 
 create table shop (
-  id                        bigint not null,
+  id                        bigserial PRIMARY KEY,
   name                      varchar(255),
   address_line1             varchar(255),
   address_line2             varchar(255),
@@ -27,44 +24,19 @@ create table shop (
   city                      varchar(255),
   town                      varchar(255),
   phone_number              varchar(255),
-  email_email               varchar(255),
-  constraint pk_shop primary key (id))
+  email                     varchar(255) REFERENCES member,
+  )
 ;
 
 
 create table product_shop (
-  product_id                     bigint not null,
-  shop_id                        bigint not null,
-  constraint pk_product_shop primary key (product_id, shop_id))
+  product_id                     bigint REFERENCES products ON DELETE RESTRICT,
+  shop_id                        bigint REFERENCES shop ON DELETE CASCADE,
+  PRIMARY KEY (product_id, shop_id))
 ;
-create sequence member_seq;
-
-create sequence product_seq;
-
-create sequence shop_seq;
-
-alter table shop add constraint fk_shop_email_1 foreign key (email_email) references member (email);
-create index ix_shop_email_1 on shop (email_email);
 
 
 
-alter table product_shop add constraint fk_product_shop_product_01 foreign key (product_id) references product (id);
 
-alter table product_shop add constraint fk_product_shop_shop_02 foreign key (shop_id) references shop (id);
 
-# --- !Downs
-
-drop table if exists member cascade;
-
-drop table if exists product cascade;
-
-drop table if exists product_shop cascade;
-
-drop table if exists shop cascade;
-
-drop sequence if exists member_seq;
-
-drop sequence if exists product_seq;
-
-drop sequence if exists shop_seq;
 
