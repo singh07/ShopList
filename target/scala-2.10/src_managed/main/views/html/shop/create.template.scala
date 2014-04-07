631b307103f41c925a5bbbf586a688f8bdb773f0
singhakash <singh_akash@outlook.com>
2014-04-07 10:29:25 +0530

    addShop working

--- target/scala-2.10/src_managed/main/views/html/shop/create.template.scala
+++ target/scala-2.10/src_managed/main/views/html/shop/create.template.scala
@@ -43,31 +43,34 @@ Seq[Any](format.raw/*1.45*/("""
 
 
 """),_display_(Seq[Any](/*19.2*/form(action = routes.ShopController.submit(), 'id -> "shopCreationForm", 'class -> "form-horizontal", 'role->"form")/*19.118*/ {_display_(Seq[Any](format.raw/*19.120*/("""
+"""),format.raw/*49.3*/("""
 
-"""),_display_(Seq[Any](/*21.2*/inputText(userForm("name"), '_label -> "Shop Name",'class -> "form-control"))),format.raw/*21.78*/("""
-"""),_display_(Seq[Any](/*22.2*/inputText(userForm("addressLine1"), '_label -> "Address Line 1",'class -> "form-control"))),format.raw/*22.91*/("""
-"""),_display_(Seq[Any](/*23.2*/inputText(userForm("addressLine2"), '_label -> "Address Line 2",'class -> "form-control"))),format.raw/*23.91*/("""
-"""),_display_(Seq[Any](/*24.2*/inputText(userForm("addressLine3"), '_label -> "Address Line 3",'class -> "form-control"))),format.raw/*24.91*/("""
-"""),_display_(Seq[Any](/*25.2*/inputText(userForm("city"), '_label -> "City",'class -> "form-control"))),format.raw/*25.73*/("""
-"""),_display_(Seq[Any](/*26.2*/inputText(userForm("town"), '_label -> "Town",'class -> "form-control"))),format.raw/*26.73*/("""
-"""),_display_(Seq[Any](/*27.2*/inputText(userForm("phoneNumber"), '_label -> "Phone",'class -> "form-control"))),format.raw/*27.81*/("""
-"""),format.raw/*28.155*/("""
+
+"""),_display_(Seq[Any](/*52.2*/inputText(userForm("name"), '_label -> "Shop Name",'class -> "form-control"))),format.raw/*52.78*/("""
+"""),_display_(Seq[Any](/*53.2*/inputText(userForm("addressLine1"), '_label -> "Address Line 1",'class -> "form-control"))),format.raw/*53.91*/("""
+"""),_display_(Seq[Any](/*54.2*/inputText(userForm("addressLine2"), '_label -> "Address Line 2",'class -> "form-control"))),format.raw/*54.91*/("""
+"""),_display_(Seq[Any](/*55.2*/inputText(userForm("addressLine3"), '_label -> "Address Line 3",'class -> "form-control"))),format.raw/*55.91*/("""
+"""),_display_(Seq[Any](/*56.2*/inputText(userForm("city"), '_label -> "City",'class -> "form-control"))),format.raw/*56.73*/("""
+"""),_display_(Seq[Any](/*57.2*/inputText(userForm("town"), '_label -> "Town",'class -> "form-control"))),format.raw/*57.73*/("""
+"""),_display_(Seq[Any](/*58.2*/inputText(userForm("phoneNumber"), '_label -> "Phone",'class -> "form-control"))),format.raw/*58.81*/("""
+
+
 
 <div class="form-group">
     <label for="exampleInputEmail1">Owner Email</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Owner email" value=""""),_display_(Seq[Any](/*32.104*/user)),format.raw/*32.108*/("""" placeholder=""""),_display_(Seq[Any](/*32.124*/user)),format.raw/*32.128*/("""" readonly>
+    <input type="email" class="form-control" id="exampleInputEmail1"  value=""""),_display_(Seq[Any](/*64.79*/user)),format.raw/*64.83*/("""" placeholder=""""),_display_(Seq[Any](/*64.99*/user)),format.raw/*64.103*/("""" readonly>
   </div>
 
-"""),format.raw/*43.5*/("""
+"""),format.raw/*76.5*/("""
 
     <div class="actions">
         <input type="submit" class="btn btn-primary" value="Create">
-        <a href=""""),_display_(Seq[Any](/*47.19*/routes/*47.25*/.ApplicationController.index)),format.raw/*47.53*/("""" class="btn">Cancel</a>
+        <a href=""""),_display_(Seq[Any](/*80.19*/routes/*80.25*/.ApplicationController.index)),format.raw/*80.53*/("""" class="btn">Cancel</a>
     </div>
 
 
 </fieldset>
-        """)))})),format.raw/*52.10*/("""
+        """)))})),format.raw/*85.10*/("""
         """)))})))}
     }
     
@@ -80,11 +83,11 @@ Seq[Any](format.raw/*1.45*/("""
 }
                 /*
                     -- GENERATED --
-                    DATE: Sun Apr 06 21:23:50 IST 2014
+                    DATE: Sun Apr 06 22:25:33 IST 2014
                     SOURCE: C:/Users/Akash Singh/Desktop/ShopList/app/views/shop/create.scala.html
-                    HASH: 8f83937d3d3d035c32a142e90329d14df817a59d
-                    MATRIX: 798->1|987->44|1017->101|1056->106|1089->131|1128->133|1159->296|1410->512|1536->628|1577->630|1617->635|1715->711|1753->714|1864->803|1902->806|2013->895|2051->898|2162->987|2200->990|2293->1061|2331->1064|2424->1135|2462->1138|2563->1217|2594->1373|2821->1563|2848->1567|2901->1583|2928->1587|2980->1895|3135->2014|3150->2020|3200->2048|3296->2112
-                    LINES: 26->1|32->1|34->5|36->7|36->7|36->7|38->12|45->19|45->19|45->19|47->21|47->21|48->22|48->22|49->23|49->23|50->24|50->24|51->25|51->25|52->26|52->26|53->27|53->27|54->28|58->32|58->32|58->32|58->32|61->43|65->47|65->47|65->47|70->52
+                    HASH: 11d6c19bac4a1747af51e951c64490d099e715fc
+                    MATRIX: 798->1|987->44|1017->101|1056->106|1089->131|1128->133|1159->296|1410->512|1536->628|1577->630|1606->1645|1648->1652|1746->1728|1784->1731|1895->1820|1933->1823|2044->1912|2082->1915|2193->2004|2231->2007|2324->2078|2362->2081|2455->2152|2493->2155|2594->2234|2799->2403|2825->2407|2877->2423|2904->2427|2956->2884|3111->3003|3126->3009|3176->3037|3272->3101
+                    LINES: 26->1|32->1|34->5|36->7|36->7|36->7|38->12|45->19|45->19|45->19|46->49|49->52|49->52|50->53|50->53|51->54|51->54|52->55|52->55|53->56|53->56|54->57|54->57|55->58|55->58|61->64|61->64|61->64|61->64|64->76|68->80|68->80|68->80|73->85
                     -- GENERATED --
                 */
             
